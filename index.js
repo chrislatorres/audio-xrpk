748d7210b62338e320e0cab86d5111c4fa24e439
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-20 16:57:48 -0600

    add wbn and updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import {scene, renderer, camera} from 'app';
+import {app, appManager, scene, renderer, camera} from 'app';
 
 // create an AudioListener and add it to the camera
 const listener = new THREE.AudioListener();
@@ -10,19 +10,19 @@ const sound = new THREE.PositionalAudio( listener );
 
 // load a sound and set it as the PositionalAudio object's buffer
 const audioLoader = new THREE.AudioLoader();
-audioLoader.load( 'sound.mp3', function( buffer ) {
-	sound.setBuffer( buffer );
-	sound.setRefDistance( 20 );
-	sound.setLoop( true );
-	sound.setVolume( 1 );
-	sound.play();
+
+audioLoader.load( app.files['./sound.mp3'], function( buffer ) {
+  sound.setBuffer( buffer );
+  sound.setRefDistance( 3 );
+  sound.setLoop( true );
+  sound.setVolume( 1 );
+  sound.play();
 });
 
 // create an object for the sound to play from
-const sphere = new THREE.SphereBufferGeometry( 20, 32, 16 );
+const sphere = new THREE.SphereBufferGeometry( 3, 3, 3 );
 const material = new THREE.MeshPhongMaterial( { color: 0xff2200 } );
 const mesh = new THREE.Mesh( sphere, material );
-scene.add( mesh );
-
-// finally add the sound to the mesh
 mesh.add( sound );
+
+app.object.add( mesh );
